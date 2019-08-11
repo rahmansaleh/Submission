59599943c7cb54616c2d86a0929e220a5c5c6279
muhammad rahman <rahman9600@gmail.com>
2019-08-11 21:51:17 +0700

    Create Product.kt

---
+++ app/src/main/java/com/example/submission/model/Product.kt
@@ -0,0 +1,9 @@
+package com.example.submission.model
+
+data class Product (
+    var img: String = "",
+    var title: String = "",
+    var tag: String = "",
+    var count_seen: String = "",
+    var cuont_sold: String = ""
+)
