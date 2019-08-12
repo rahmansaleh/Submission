dde580c9ba9a2f31b22715f502d4d2e621584c90
muhammad rahman <rahman9600@gmail.com>
2019-08-13 03:50:41 +0700

    Update MainActivity.kt
    - init ui, data, databinding, action for adapter

--- app/src/main/java/com/example/submission/MainActivity.kt
+++ app/src/main/java/com/example/submission/MainActivity.kt
@@ -2,11 +2,50 @@ package com.example.submission
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.ActionBar
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.widget.Toast
+import com.example.submission.Adapter.CardViewProductAdapter
+import com.example.submission.model.Product
+import com.example.submission.model.ProductData
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var rv_main: RecyclerView
+    private var list: ArrayList<Product> = arrayListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setActionBarTitle()
+
+        rv_main = findViewById(R.id.rv_main)
+        rv_main.setHasFixedSize(true)
+
+        list.addAll(ProductData.listData)
+        showRecyclerCardView()
+    }
+
+    private fun setActionBarTitle() {
+        if( supportActionBar != null ) {
+            (supportActionBar as ActionBar).title = null
+        }
+    }
+
+    private fun showRecyclerCardView() {
+        rv_main.layoutManager = LinearLayoutManager(this)
+        val cardViewHeroAdapter = CardViewProductAdapter(list)
+        rv_main.adapter = cardViewHeroAdapter
+
+        cardViewHeroAdapter.setOnItemClickCallBack(object : CardViewProductAdapter.OnItemClickCallBack{
+            override fun onItemClicked(data: Product) {
+                showSelectedHero(data)
+            }
+        })
+    }
+
+    private fun showSelectedHero(product: Product) {
+        Toast.makeText(this, product.title, Toast.LENGTH_SHORT).show()
     }
 }
