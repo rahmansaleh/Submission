ab3b897fe0557d1fd2847313af94001074c5a240
muhammad rahman <rahman9600@gmail.com>
2019-08-13 03:49:49 +0700

    Create DetailProduct.kt

---
+++ app/src/main/java/com/example/submission/DetailProduct.kt
@@ -0,0 +1,12 @@
+package com.example.submission
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailProduct : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_product)
+    }
+}
