be2cb5b7ebffa1e388fd2a9636d15601bfbc0285
muhammad rahman <rahman9600@gmail.com>
2019-08-13 10:23:33 +0700

    Update DetailProduct.kt
    - adding title

--- app/src/main/java/com/example/submission/DetailProduct.kt
+++ app/src/main/java/com/example/submission/DetailProduct.kt
@@ -2,11 +2,20 @@ package com.example.submission
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.ActionBar
 
 class DetailProduct : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_product)
+
+        setActionBarTitle("Phone Detail")
+    }
+
+    private fun setActionBarTitle(title: String) {
+        if( supportActionBar != null ) {
+            (supportActionBar as ActionBar).title = title
+        }
     }
 }
