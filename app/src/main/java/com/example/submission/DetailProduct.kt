4a79de79ae8ac03e28496f550a49ccb4d4bbfedd
muhammad rahman <rahman9600@gmail.com>
2019-08-15 21:56:10 +0700

    Update DetailProduct.kt
    - cast databinding

--- app/src/main/java/com/example/submission/DetailProduct.kt
+++ app/src/main/java/com/example/submission/DetailProduct.kt
@@ -6,6 +6,8 @@ import android.support.v7.app.ActionBar
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.example.submission.model.Parcelable.ProductParcel
 
 class DetailProduct : AppCompatActivity() {
@@ -16,9 +18,9 @@ class DetailProduct : AppCompatActivity() {
         setActionBarTitle("Phone Detail")
 
         val bundle = intent.getBundleExtra("bundle")
-        val ProductParcel = bundle.getParcelable<ProductParcel>("productParcel")
+        val productParcel = bundle.getParcelable<ProductParcel>("productParcel")
 
-        val iv_product: ImageView = findViewById(R.id.iv_photo)
+        val iv_product: ImageView = findViewById(R.id.iv_product)
         val tv_title: TextView = findViewById(R.id.tv_title)
         val tv_description: TextView = findViewById(R.id.tv_description)
         val tv_display: TextView = findViewById(R.id.tv_display)
@@ -35,6 +37,25 @@ class DetailProduct : AppCompatActivity() {
         val tv_cpuplatform: TextView = findViewById(R.id.tv_cpuplatform)
         val tv_gpuplatform: TextView = findViewById(R.id.tv_gpuplatform)
 
+        Glide.with(applicationContext)
+            .load(productParcel.img)
+            .apply(RequestOptions().override(190))
+            .into(iv_product)
+
+        tv_title.text = productParcel.title
+        tv_description.text = productParcel.description
+        tv_display.text = productParcel.size_display
+        tv_camera.text = productParcel.camera
+        tv_battery.text = productParcel.battery
+        tv_announcedlaunch.text = productParcel.announced_launch
+        tv_statuslaunch.text = productParcel.status_launch
+        tv_typesdisplay.text = productParcel.type_display
+        tv_sizedisplay.text = productParcel.size_display
+        tv_resolutiondisplay.text = productParcel.resolution_display
+        tv_osplatform.text = productParcel.os_platform
+        tv_chipsetdisplay.text = productParcel.chipset_platform
+        tv_cpuplatform.text = productParcel.cpu_platform
+        tv_gpuplatform.text = productParcel.gpu_platform
 
 
     }
