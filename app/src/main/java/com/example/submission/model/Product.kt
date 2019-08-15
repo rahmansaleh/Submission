b40ffd75a832145e0d40e69dbb21460ec85f6783
muhammad rahman <rahman9600@gmail.com>
2019-08-15 09:38:27 +0700

    Update Product.kt

--- app/src/main/java/com/example/submission/model/Product.kt
+++ app/src/main/java/com/example/submission/model/Product.kt
@@ -3,7 +3,21 @@ package com.example.submission.model
 data class Product (
     var img: String = "",
     var title: String = "",
+    var description: String = "",
     var tag: String = "",
     var count_seen: String = "",
-    var cuont_sold: String = ""
+    var cuont_sold: String = "",
+    var schreen_size: String = "",
+    var camera: String = "",
+    var ram: String = "",
+    var battery: String = "",
+    var announced_launch: String = "",
+    var status_launch: String = "",
+    var type_display: String = "",
+    var size_display: String = "",
+    var resolution_display: String = "",
+    var os_platform: String = "",
+    var chipset_platform: String = "",
+    var gpu_platform: String = "",
+    var cpu_platform: String = ""
 )
