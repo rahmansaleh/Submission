aabe294ab8c2fbd5c67cec9492cb53f9bd072aca
muhammad rahman <rahman9600@gmail.com>
2019-08-15 09:38:30 +0700

    Update ProductParcel.kt

--- app/src/main/java/com/example/submission/model/Parcelable/ProductParcel.kt
+++ app/src/main/java/com/example/submission/model/Parcelable/ProductParcel.kt
@@ -3,8 +3,42 @@ package com.example.submission.model.Parcelable
 import android.os.Parcel
 import android.os.Parcelable
 
-class ProductParcel(val img: String, val title: String, val tag: String, val count_Seen: String, val count_sold: String): Parcelable {
+class ProductParcel(
+    val img: String ,
+    val title: String ,
+    val description: String ,
+    val tag: String ,
+    val count_seen: String ,
+    val cuont_sold: String ,
+    val schreen_size: String ,
+    val camera: String ,
+    val ram: String ,
+    val battery: String ,
+    val announced_launch: String ,
+    val status_launch: String ,
+    val type_display: String ,
+    val size_display: String ,
+    val resolution_display: String ,
+    val os_platform: String ,
+    val chipset_platform: String ,
+    val gpu_platform: String ,
+    val cpu_platform: String 
+): Parcelable {
     constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
         parcel.readString(),
         parcel.readString(),
         parcel.readString(),
@@ -16,9 +50,23 @@ class ProductParcel(val img: String, val title: String, val tag: String, val cou
     override fun writeToParcel(parcel: Parcel, flags: Int) {
         parcel.writeString(img)
         parcel.writeString(title)
+        parcel.writeString(description)
         parcel.writeString(tag)
-        parcel.writeString(count_Seen)
-        parcel.writeString(count_sold)
+        parcel.writeString(count_seen)
+        parcel.writeString(cuont_sold)
+        parcel.writeString(schreen_size)
+        parcel.writeString(camera)
+        parcel.writeString(ram)
+        parcel.writeString(battery)
+        parcel.writeString(announced_launch)
+        parcel.writeString(status_launch)
+        parcel.writeString(type_display)
+        parcel.writeString(size_display)
+        parcel.writeString(resolution_display)
+        parcel.writeString(os_platform)
+        parcel.writeString(chipset_platform)
+        parcel.writeString(gpu_platform)
+        parcel.writeString(cpu_platform)
     }
 
     override fun describeContents(): Int {
@@ -34,5 +82,4 @@ class ProductParcel(val img: String, val title: String, val tag: String, val cou
             return arrayOfNulls(size)
         }
     }
-
 }
