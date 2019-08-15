d0889ec52d26c110f80528803e7a1d9c441a3c2d
muhammad rahman <rahman9600@gmail.com>
2019-08-15 08:30:40 +0700

    Update AboutUs.kt
    - adding back button in action bar

--- app/src/main/java/com/example/submission/AboutUs.kt
+++ app/src/main/java/com/example/submission/AboutUs.kt
@@ -2,11 +2,29 @@ package com.example.submission
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.ActionBar
 
 class AboutUs : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about_us)
+        setActionBarTitle("About Me")
+    }
+
+    private fun setActionBarTitle( title: String ) {
+        if( supportActionBar != null ) {
+            (supportActionBar as ActionBar).title = title
+            (supportActionBar as ActionBar).setDisplayHomeAsUpEnabled(true)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true;
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
     }
 }
