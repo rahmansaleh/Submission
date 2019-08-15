f77913df4842deab549e8dad7f0208b0fe82b1c9
muhammad rahman <rahman9600@gmail.com>
2019-08-15 08:30:55 +0700

    Update MainActivity.kt
    - adding aboutus button in action button

--- app/src/main/java/com/example/submission/MainActivity.kt
+++ app/src/main/java/com/example/submission/MainActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.support.v7.app.ActionBar
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import com.example.submission.Adapter.CardViewProductAdapter
 import com.example.submission.model.Product
@@ -28,6 +30,16 @@ class MainActivity : AppCompatActivity() {
         showRecyclerCardView()
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.mainact_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        setMode(item!!.itemId)
+        return super.onOptionsItemSelected(item)
+    }
+
     private fun setActionBarTitle() {
         if( supportActionBar != null ) {
             (supportActionBar as ActionBar).title = null
@@ -48,5 +60,13 @@ class MainActivity : AppCompatActivity() {
 
     private fun moveActivity(product: Product) {
         startActivity(Intent(this, DetailProduct::class.java))
+    }
+
+    private fun setMode(id: Int) {
+        when(id) {
+            R.id.nav_aboutus -> {
+                startActivity(Intent(this@MainActivity, AboutUs::class.java))
+            }
+        }
     }
 }
